5c8b9484c132dcbad93af6cc4a8b329385af0f98
qiuz <zhu_anly@163.com>
2019-02-14 14:37:50 +0800

    count-down & dash board

--- src/component/count-down/index.tsx
+++ src/component/count-down/index.tsx
@@ -8,7 +8,7 @@
 
 import './index.less';
 
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import classNames from 'classnames';
 
 interface propTypes {
@@ -28,160 +28,236 @@ interface propTypes {
   onTimeUp?: (...args: any) => void
 }
 
-export default class CountDown extends React.Component<propTypes, any> {
-  static defaultProps = {
-    className: '',
-    isCard: false,
-    isShowDay: false,
-    format: {
-      day: '',
-      hours: '',
-      minutes: '',
-      seconds: ''
-    },
-    day: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    onTimeUp: () => {}
-  }
+function formatNum(num: number) {
+	return num <= 9 ? `0${num}` : `${num}`
+}
 
-  seconds = 0;
-  timer: any = null;
+const defaultProps = {
+	className: '',
+	isCard: false,
+	isShowDay: false,
+	format: {
+		day: '',
+		hours: '',
+		minutes: '',
+		seconds: ''
+	},
+	day: 0,
+	hours: 0,
+	minutes: 0,
+	seconds: 0,
+	onTimeUp: () => {}
+}
 
-  constructor (props: propTypes) {
-    super(props);
-    const { day, hours, minutes, seconds } = this.props;
-    this.seconds = (day * 60 * 60 * 24) + (hours * 60 * 60) + (minutes * 60) + seconds
-    this.state = { day, hours, minutes, seconds };
-  }
 
-  formatNum (num: number) {
-    return num <= 9 ? `0${num}` : `${num}`
-  }
+export default function CountDown(props: propTypes = defaultProps) {
+	const data = { ...defaultProps, ...props };
+	let { day, hours, minutes, seconds, isCard, className, format, isShowDay } = data;
+	let sumSeconds = (day * 60 * 60 * 24) + (hours * 60 * 60) + (minutes * 60) + seconds;
+	const [state, setState] = useState({day, hours, minutes, seconds});
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (sumSeconds >= 0) {
+				const
+					d = Math.floor(sumSeconds / (60 * 60 * 24)),
+					h = Math.floor(sumSeconds / (60 * 60)) - (d * 24),
+					m = Math.floor(sumSeconds / 60) - (d * 24 * 60) - (h * 60),
+					s = Math.floor(sumSeconds) - (d * 24 * 60 * 60) - (h * 60 * 60) - (m * 60);
+				setState({ day: d, hours: h, minutes: m, seconds: s });
+			}
+			sumSeconds--;
+			if (sumSeconds < 0) {
+				clearInterval(timer);
+				data.onTimeUp();
+			}
+		}, 1000);
+		return () => clearInterval(timer);
+	}, [sumSeconds])
 
-  componentWillMount() {
-    this.countDown();
-  }
-
-  componentDidMount () {
-    this.timer = setInterval(() => {
-     this.countDown();
-    }, 1000)
-  }
-
-  countDown() {
-    let [day, hours, minutes, seconds] = [0, 0, 0, 0]
-    if (this.seconds > 0) {
-      day = Math.floor(this.seconds / (60 * 60 * 24))
-      hours = Math.floor(this.seconds / (60 * 60)) - (day * 24)
-      minutes = Math.floor(this.seconds / 60) - (day * 24 * 60) - (hours * 60)
-      seconds = Math.floor(this.seconds) - (day * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
-    }
-    this.setState({ day, hours, minutes, seconds })
-    this.seconds--
-    if (this.seconds < 0) {
-      clearInterval(this.timer)
-      this.timer = null
-      this.props.onTimeUp()
-    }
-	}
-	
-	componentWillReceiveProps(props: propTypes) {
-		const {
-      day,
-      hours,
-      minutes,
-      seconds
-		} = this.state;
-		if (props.day !== day || props.hours !== hours || props.minutes !== minutes || props.seconds !== seconds) {
-			const { day, hours, minutes, seconds } = props;
-	    this.seconds = (day * 60 * 60 * 24) + (hours * 60 * 60) + (minutes * 60) + seconds
-			this.setState({
-				day,
-				hours,
-				minutes,
-				seconds
-			}, () => {
-				clearInterval(this.timer);
-				this.timer = setInterval(() => {
-					this.countDown()
-				 }, 1000);
-			});
-		}
-	}
-
-  componentWillUnmount () {
-    if (this.timer) {
-      clearInterval(this.timer)
-    }
-  }
-
-  render () {
-    const {
-      className,
-      format,
-      isShowDay,
-      isCard
-    } = this.props
-    const {
-      day,
-      hours,
-      minutes,
-      seconds
-    } = this.state
-
-    return (
-      <div
-        className={
-          classNames({
-            'cmp-count-down': true,
-            'cmp-count-down--card': isCard
-          }, className)}
-      >
-        {
-          isShowDay
-            ? <div className='cmp-count-down__item'>
-              <div className='cmp-count-down__time-box'>
-                <span className='cmp-count-down__time'>
-                  {this.formatNum(day)}
-                </span>
-              </div>
-              <span className='cmp-count-down__separator'>{format.day}</span>
-            </div>
-            : null
-        }
-        {
-          format.hours &&
-          <div className='cmp-count-down__item'>
+  return (
+    <div
+      className={
+        classNames({
+          'cmp-count-down': true,
+          'cmp-count-down--card': isCard
+        }, className)}
+    >
+      {
+        isShowDay
+          ? <div className='cmp-count-down__item'>
             <div className='cmp-count-down__time-box'>
               <span className='cmp-count-down__time'>
-                {this.formatNum(hours)}
+                {formatNum(state.day)}
               </span>
             </div>
-            <span className='cmp-count-down__separator'>{format.hours}</span>
+            <span className='cmp-count-down__separator'>{format.day}</span>
           </div>
-        }
-        {
-          format.minutes &&
-          <div className='cmp-count-down__item'>
-            <div className='cmp-count-down__time-box'>
-              <span className='cmp-count-down__time'>
-                {this.formatNum(minutes)}
-              </span>
-            </div>
-            <span className='cmp-count-down__separator'>{format.minutes}</span>
-          </div>
-        }
+          : null
+      }
+      {
+        format.hours &&
         <div className='cmp-count-down__item'>
           <div className='cmp-count-down__time-box'>
             <span className='cmp-count-down__time'>
-              {this.formatNum(seconds)}
+              {formatNum(state.hours)}
             </span>
           </div>
-          <span className='cmp-count-down__separator'>{format.seconds}</span>
+          <span className='cmp-count-down__separator'>{format.hours}</span>
         </div>
+      }
+      {
+        format.minutes &&
+        <div className='cmp-count-down__item'>
+          <div className='cmp-count-down__time-box'>
+            <span className='cmp-count-down__time'>
+              {formatNum(state.minutes)}
+            </span>
+          </div>
+          <span className='cmp-count-down__separator'>{format.minutes}</span>
+        </div>
+      }
+      <div className='cmp-count-down__item'>
+        <div className='cmp-count-down__time-box'>
+          <span className='cmp-count-down__time'>
+            {formatNum(state.seconds)}
+          </span>
+        </div>
+        <span className='cmp-count-down__separator'>{format.seconds}</span>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+// export default class CountDown extends React.Component<propTypes, any> {
+//   static defaultProps = {
+//     className: '',
+//     isCard: false,
+//     isShowDay: false,
+//     format: {
+//       day: '',
+//       hours: '',
+//       minutes: '',
+//       seconds: ''
+//     },
+//     day: 0,
+//     hours: 0,
+//     minutes: 0,
+//     seconds: 0,
+//     onTimeUp: () => {}
+//   }
+
+//   seconds = 0;
+//   timer: any = null;
+
+//   constructor (props: propTypes) {
+// 		super(props);
+//     const { day, hours, minutes, seconds } = this.props;
+//     this.seconds = (day * 60 * 60 * 24) + (hours * 60 * 60) + (minutes * 60) + seconds
+//     this.state = { day, hours, minutes, seconds };
+//   }
+
+//   formatNum (num: number) {
+//     return num <= 9 ? `0${num}` : `${num}`
+//   }
+
+//   componentWillMount() {
+//     this.countDown();
+//   }
+
+//   componentDidMount () {
+//     this.timer = setInterval(() => {
+//      this.countDown();
+//     }, 1000)
+//   }
+
+//   countDown() {
+//     let [day, hours, minutes, seconds] = [0, 0, 0, 0]
+//     if (this.seconds > 0) {
+//       day = Math.floor(this.seconds / (60 * 60 * 24))
+//       hours = Math.floor(this.seconds / (60 * 60)) - (day * 24)
+//       minutes = Math.floor(this.seconds / 60) - (day * 24 * 60) - (hours * 60)
+//       seconds = Math.floor(this.seconds) - (day * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
+// 		}
+//     this.setState({ day, hours, minutes, seconds })
+//     this.seconds--
+//     if (this.seconds < 0) {
+//       clearInterval(this.timer)
+//       this.timer = null
+//       this.props.onTimeUp()
+//     }
+// 	}
+
+//   componentWillUnmount () {
+//     if (this.timer) {
+//       clearInterval(this.timer)
+//     }
+//   }
+
+//   render () {
+//     const {
+//       className,
+//       format,
+//       isShowDay,
+//       isCard
+//     } = this.props
+//     const {
+//       day,
+//       hours,
+//       minutes,
+//       seconds
+//     } = this.state
+
+//     return (
+//       <div
+//         className={
+//           classNames({
+//             'cmp-count-down': true,
+//             'cmp-count-down--card': isCard
+//           }, className)}
+//       >
+//         {
+//           isShowDay
+//             ? <div className='cmp-count-down__item'>
+//               <div className='cmp-count-down__time-box'>
+//                 <span className='cmp-count-down__time'>
+//                   {this.formatNum(day)}
+//                 </span>
+//               </div>
+//               <span className='cmp-count-down__separator'>{format.day}</span>
+//             </div>
+//             : null
+//         }
+//         {
+//           format.hours &&
+//           <div className='cmp-count-down__item'>
+//             <div className='cmp-count-down__time-box'>
+//               <span className='cmp-count-down__time'>
+//                 {this.formatNum(hours)}
+//               </span>
+//             </div>
+//             <span className='cmp-count-down__separator'>{format.hours}</span>
+//           </div>
+//         }
+//         {
+//           format.minutes &&
+//           <div className='cmp-count-down__item'>
+//             <div className='cmp-count-down__time-box'>
+//               <span className='cmp-count-down__time'>
+//                 {this.formatNum(minutes)}
+//               </span>
+//             </div>
+//             <span className='cmp-count-down__separator'>{format.minutes}</span>
+//           </div>
+//         }
+//         <div className='cmp-count-down__item'>
+//           <div className='cmp-count-down__time-box'>
+//             <span className='cmp-count-down__time'>
+//               {this.formatNum(seconds)}
+//             </span>
+//           </div>
+//           <span className='cmp-count-down__separator'>{format.seconds}</span>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
--- src/container/dashboard/index.tsx
+++ src/container/dashboard/index.tsx
@@ -6,23 +6,75 @@
  * @Last Modified time: 2019-02-13 09:46:59
  */
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import './index.less';
 import { dateFormat } from 'src/common/utils';
+import CountDown from 'src/component/count-down';
+import logo from 'src/assets/images/logo.svg';
+import { Button } from 'antd-mobile';
 
 export default function Dashboard() {
 	const [time, setTime] = useState(new Date());
-
+	const [countTime, setCountTime] = useState({minutes: 1, seconds: 0});
+	const CountDownView = useMemo(() => <CountDown format={{ minutes: ':', seconds: '' }} minutes={countTime.minutes} seconds={countTime.seconds} />, [countTime.seconds, countTime.minutes])
 	useEffect(() => {
 		setTime(new Date());
-		const timer = setInterval(() => setTime(new Date()), 1000);
+		const timer = setInterval(() => {
+			setTime(new Date());
+		}, 1000);
 		return () => clearInterval(timer);
-	})
+	}, [time])
 	return (
 		<div className="dashboard">
-			<p className="dashboard-intro">
-				{dateFormat(time)}
+			<div className="header-logo">
+				<img src={logo} className="logo" alt="logo" />
+				<p className="title">React Touch Base</p>
+			</div>
+			
+			<p className="now">
+				Now: {dateFormat(time)}
 			</p>
+			<div className="count-down">
+				30 minutes countdown {CountDownView}
+			</div>
+
+			<div className="content">
+				<Button className="btn">List</Button>
+			</div>
 		</div>
 	)
 }
+
+// export default class extends React.Component<any, any> {
+// 	timer: any;
+// 	constructor(props: any) {
+// 		super(props);
+// 		this.state = {
+// 			time: new Date(),
+// 			countTime: {
+// 				minutes: 30,
+// 				seconds: 0
+// 			}
+// 		}
+// 	}
+
+// 	componentDidMount() {
+// 		this.timer = setInterval(() => this.setState({time: new Date()}), 1000);
+// 	}
+
+// 	componentWillUnmount() {
+// 		clearInterval(this.timer);
+// 	}
+
+//   public render() {
+// 		const { time, countTime } = this.state;
+//     return (
+//       <div className="dashboard">
+//         <p className="dashboard-intro">
+// 					{dateFormat(time)}
+//         </p>
+// 				<CountDown  format={{ minutes: ':', seconds: '' }} minutes={countTime.minutes} seconds={countTime.seconds} />
+//       </div>
+//     );
+//   }
+// }
